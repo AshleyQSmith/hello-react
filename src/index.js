48ac1e3c0f28fd961a34622f7455950827069571
AshleyQSmith <smittyq@gmail.com>
2020-07-24 13:20:49 -0700

    nested elements

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 ReactDOM.render(
-  React.createElement("h1", { style: { color: "blue" } }, "Hello!"),
+  React.createElement(
+    "ul",
+    { style: { color: "blue" } },
+    React.createElement("li", null, "hotdogs"),
+    React.createElement("li", null, "hamburgers"),
+    React.createElement("li", null, "pizza")
+  ),
+
+  // React.createElement("h1", { style: { color: "blue" } }, "Hello!"),
+
   // React.createElement("h1", null, "hello-react!"),
+
   document.getElementById("root")
 );
