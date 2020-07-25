8e881c5073cf8c122a8515b93bc3c46a4e930016
AshleyQSmith <smittyq@gmail.com>
2020-07-25 13:11:24 -0700

    conditionally rendered component & refactored

--- src/index.js
+++ src/index.js
@@ -2,51 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// RENDERING LISTS
+// CONDITIONAL RENDERING
 
-// const lakeList = ["Echo Lake", "Cascade Lake", "Maud Lake"];
-
-// function App(props) {
-//   return (
-//     <ul>
-//       {props.lakes.map((lake) => (
-//         <li>{lake}</li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// or DESTRUCTURED VERSION:
-// function App({ lakes }) {
-//   return (
-//     <ul>
-//       {lakes.map((lake) => (
-//         <li>{lake}</li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// RENDERING LISTS OF OBJECTS + ADDING KEYS: identifier for dynamically rendered elemtent, in this case the id (gets rid of console error)
-// FYI FOR ARRAY OF NUMBERS TO BE RENDERED WITHOUT ID, YOU CAN SET THE key={item.toString()}
-
-const lakeList = [
-  { id: "1", name: "Echo", trailhead: "Echo" },
-  { id: "2", name: "Velma", trailhead: "Bayview" },
-  { id: "3", name: "Maud", trailhead: "Wrights" },
-];
-
-function App({ lakes }) {
+function Lake({ name }) {
   return (
     <div>
-      {lakes.map((lake) => (
-        <div key={lake.id}>
-          <h1>{lake.name} Lake</h1>
-          <p>Accessed by {lake.trailhead} Trailhead</p>
-        </div>
-      ))}
+      <h1>Visit {name} Lake!</h1>
     </div>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name} Resort!</h1>
+    </div>
+  );
+}
+
+// function App(props) {
+//   if (props.season === "summer") {
+//     return <Lake name="Jenny" />;
+//   } else if (props.season === "winter") {
+//     return <SkiResort name="Jackson Hole Mountain" />;
+//   }
+// }
+
+// REFACTORED OPTION
+
+function App(props) {
+  return (
+    <div>
+      {props.season === "summer" ? (
+        <Lake name="Jenny" />
+      ) : props.season === "winter" ? (
+        <SkiResort name="Jackson Hole" />
+      ) : (
+        <h1>Come back in summer or winter!</h1>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App season="summer" />, document.getElementById("root"));
