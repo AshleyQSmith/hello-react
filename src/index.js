3f2eeadfad43238d881c8ef3484072e001c6b309
AshleyQSmith <smittyq@gmail.com>
2020-08-02 14:30:14 -0600

    built in hook/function "useState"

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// CONDITIONAL RENDERING
-
-function Lake({ name }) {
+function App() {
+  const [status, setStatus] = useState("Open");
   return (
     <div>
-      <h1>Visit {name} Lake!</h1>
+      <h1>Status: {status} </h1>
+      <button onClick={() => setStatus("Closed")}>Closed</button>
+      <button onClick={() => setStatus("Open")}>Open</button>
+      <button onClick={() => setStatus("Back in 5")}>Break</button>
     </div>
   );
 }
 
-function SkiResort({ name }) {
-  return (
-    <div>
-      <h1>Visit {name} Resort!</h1>
-    </div>
-  );
-}
-
-// function App(props) {
-//   if (props.season === "summer") {
-//     return <Lake name="Jenny" />;
-//   } else if (props.season === "winter") {
-//     return <SkiResort name="Jackson Hole Mountain" />;
-//   }
-// }
-
-// REFACTORED OPTION
-
-function App(props) {
-  return (
-    <div>
-      {props.season === "summer" ? (
-        <Lake name="Jenny" />
-      ) : props.season === "winter" ? (
-        <SkiResort name="Jackson Hole" />
-      ) : (
-        <h1>Come back in summer or winter!</h1>
-      )}
-    </div>
-  );
-}
-
-ReactDOM.render(<App season="summer" />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
