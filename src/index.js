2a4dca71afdd922fcba650da0382c48454f9bd44
AshleyQSmith <smittyq@gmail.com>
2020-08-03 16:21:46 -0600

    fetching data with useEffect hook

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function GithubUser({ login }) {
+  const [data, setData] = useState(null);
+  // fetching data with useEffect
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        {JSON.stringify(data)}
+        <h1>User: {data.login}</h1>
+        <h3>Name: {data.name}</h3>
+        <img src={data.avatar_url} width={100} />
+      </div>
+    );
+  }
+  return null;
+}
+
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  // dependancy array added to useEffect - second argument sent. its the state variable we want to listen to changes in - limits scope/smart rendering.
-
-  useEffect(() => {
-    console.log(`field 1: ${val}`);
-  }, [val]);
-  useEffect(() => {
-    console.log(`field 2: ${val2}`);
-  }, [val2]);
-  return (
-    <>
-      <label>
-        Fave phrase: {val}
-        <input value={val} onChange={(e) => setVal(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        2nd Fave phrase: {val2}
-        <input value={val2} onChange={(e) => setVal2(e.target.value)} />
-      </label>
-    </>
-  );
+  return <GithubUser login="ashleyqsmith" />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
